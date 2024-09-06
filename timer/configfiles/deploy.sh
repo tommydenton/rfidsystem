52549c1c8a8a19b4f97870441632f66881f6e4a5
Thomas Denton <tommydenton@gmail.com>
2024-09-05 22:13:00 -0500

    updated deploy

--- timer/configfiles/deploy.sh
+++ timer/configfiles/deploy.sh
@@ -3,7 +3,7 @@
 
 # Copy the files from the source to the target directory
 echo "Copying files from $SOURCE_DIR to $TARGET_DIR..."
-sudo cp -R /home/pi/html/rfidsystem/timer/*  /var/www/html/timer
+sudo cp -R /home/pi//rfidsystem/timer/*  /var/www/html/timer
 
 # Restart the services using PM2
 echo "Restarting services..."
@@ -11,8 +11,8 @@ pm2 restart /var/www/html/timer/ntpapi/ntpapi.mjs
 pm2 restart /var/www/html/timer/display/server.js
 
 # Check the Git status
-echo "Checking Git status..."
-git -C /home/pi/html/rfidsystem/timer status
+#echo "Checking Git status..."
+#git -C /home/pi/html/rfidsystem/timer status
 
 echo "Deployment completed."
 
