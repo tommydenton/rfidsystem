8b61a0364182f8977eb666e85db396bdfe228691
Thomas Denton <tommydenton@gmail.com>
2024-09-17 21:03:00 -0500

    boats showing

--- timer/display/server.js
+++ timer/display/server.js
@@ -176,16 +176,6 @@ app.get('/display', async (req, res, next) => {
             GROUP BY b.boatnumber
         `);
 
-        const minMaxBoat = await pool.query(`
-            SELECT b.boatnumber, MIN(t.timestamp_h) AS min_timestamp, MAX(t.timestamp_h) AS max_timestamp
-            FROM BOATS b
-            JOIN LINKER l ON b.bibnumber1 = l.bibnumber OR b.bibnumber2 = l.bibnumber
-            JOIN TIMERESULTS t ON l.tag_id = t.tag_id
-            GROUP BY b.boatnumber
-            ORDER BY min_timestamp ASC, max_timestamp DESC
-            LIMIT 1
-        `);
-
         const allTogether = await pool.query(`
             SELECT d.bibnumber, d.fname, d.lname, b.boatnumber, l.tag_id, MIN(t.timestamp_h) AS first_timestamp, MAX(t.timestamp_h) AS last_timestamp
             FROM DEMODATA d
@@ -195,17 +185,27 @@ app.get('/display', async (req, res, next) => {
             GROUP BY d.bibnumber, d.fname, d.lname, b.boatnumber, l.tag_id
         `);
 
+        const minMaxBoat = await pool.query(`
+            SELECT b.boatnumber, MIN(t.timestamp_h) AS min_timestamp, MAX(t.timestamp_h) AS max_timestamp
+            FROM BOATS b
+            JOIN LINKER l ON b.bibnumber1 = l.bibnumber OR b.bibnumber2 = l.bibnumber
+            JOIN TIMERESULTS t ON l.tag_id = t.tag_id
+            GROUP BY b.boatnumber
+            ORDER BY min_timestamp ASC, max_timestamp DESC
+        `);
+
         // Log the results to ensure they are correct
         // console.log('racerData:', JSON.stringify(racerData.rows, null, 2));
         // console.log('boatTime:', JSON.stringify(boatTime.rows, null, 2));
         // console.log('minMaxBoat:', JSON.stringify(minMaxBoat.rows, null, 2));
         // console.log('allTogether:', JSON.stringify(allTogether.rows, null, 2));
+        console.log(minMaxBoat.rows);
 
         res.render('index', {
             racerData: racerData.rows,
             boatTime: boatTime.rows,
-            minMaxBoat: minMaxBoat.rows.length > 0 ? minMaxBoat.rows[0] : null, // Handle case where no result is returned
-            allTogether: allTogether.rows
+            allTogether: allTogether.rows,
+            minMaxBoat: minMaxBoat.rows
         });
     } catch (error) {
         console.error('Detailed error:', error);
